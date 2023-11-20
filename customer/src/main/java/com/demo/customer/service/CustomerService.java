86ffb97e670456e5ca4f7b78bc78a1c87f33faf6
christian.juina <Nirvana2022cce>
2023-11-20 18:36:54 -0500

    servicios

--- customer/src/main/java/com/demo/customer/service/CustomerService.java
+++ customer/src/main/java/com/demo/customer/service/CustomerService.java
@@ -1,11 +1,9 @@
 package com.demo.customer.service;
 import com.demo.customer.entity.Customer;
-import com.demo.customer.repository.CustomerRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -14,9 +12,6 @@ public class CustomerService {
 
     @PersistenceContext
     EntityManager em;
-
-    @Autowired
-    private CustomerRepository customerRepository;
 
     public Customer findByName(String name) {
         String consulta = "SELECT c FROM Customer c WHERE c.name = :name";
