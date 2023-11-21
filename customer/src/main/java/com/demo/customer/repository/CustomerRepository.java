e514b361e118785a8ebe0ea2a4c90c35c8c141de
christian.juina <Nirvana2022cce>
2023-11-21 14:27:47 -0500

    query, jpaRepository

--- customer/src/main/java/com/demo/customer/repository/CustomerRepository.java
+++ customer/src/main/java/com/demo/customer/repository/CustomerRepository.java
@@ -2,6 +2,10 @@ package com.demo.customer.repository;
 
 import com.demo.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Query("SELECT c FROM Customer c WHERE c.name = :name")
+    public Customer findByName(String name);
 }
