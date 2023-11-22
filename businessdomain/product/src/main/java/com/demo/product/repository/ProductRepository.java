5a5c57f94c143bae2333cb9ea781c4260a2fc398
christian.juina <Nirvana2022cce>
2023-11-22 15:45:35 -0500

    reestructura del proyecto

--- businessdomain/product/src/main/java/com/demo/product/repository/ProductRepository.java
+++ businessdomain/product/src/main/java/com/demo/product/repository/ProductRepository.java
@@ -1,10 +1,7 @@
 package com.demo.product.repository;
 
 import com.demo.product.entity.Product;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
