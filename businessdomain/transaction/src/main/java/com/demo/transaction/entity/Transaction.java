da89cfefcd9331172e64456d71799a17ee02295d
christian.juina <Nirvana2022cce>
2023-12-13 18:37:00 -0500

    transaction

--- businessdomain/transaction/src/main/java/com/demo/transaction/entity/Transaction.java
+++ businessdomain/transaction/src/main/java/com/demo/transaction/entity/Transaction.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.Date;
 
 @Entity
+@Table(name = "transaction")
 @Data
 public class Transaction {
     @Id
